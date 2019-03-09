85f93fffdd01029c35f7b7539c589392169821a1
jimepholmes <jim_e_p_holmes@hotmail.com>
2019-03-09 10:46:34 +0000

    refactoring for redux

---
+++ redux_messaging/src/containers/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Sidebar from '../components/Sidebar';
+import Main from '../components/Main';
+import './App.css';
+import store from '../store';
+import _ from 'lodash';
+
+const App = () => {
+  const { contacts } = store.getState();
+  console.log(contacts);
+
+  return (
+    <div className="App">
+      <Sidebar contacts={_.values(contacts)} />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ redux_messaging/src/reducers/index.js
@@ -0,0 +1,8 @@
+import user from "./user";
+import contacts from "./contacts";
+import { combineReducers } from 'redux';
+
+export default  combineReducers({
+    user,
+    contacts
+})
