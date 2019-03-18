0182f50b77a847a6c35017ffa9302fec48b9b257
jimepholmes <jim_e_p_holmes@hotmail.com>
2019-03-18 20:09:36 +0000

    finished hooking up everything to redux

--- redux_messaging/src/components/ChatWindow.js
+++ redux_messaging/src/components/ChatWindow.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import store from '../store';
+import Header from '../components/Header';
+import Chats from './Chats'; 
+import MessageInput from '../containers/MessageInput';
 import './ChatWindow.css';
+import _ from 'lodash';
 
 const ChatWindow = ({ activeUserId }) => {
-    return (
-      <div className="ChatWindow">Conversation for user id: {activeUserId}</div>
+  const state = store.getState();
+  //const activeUser = state.contacts[activeUserId];
+  //const activeUser = _.values(_.values(state.contacts)[0][activeUserId]);
+  const activeUser = _.values(state.contacts)[0][activeUserId];
+  const messages = state.messages[activeUserId];
+  const { typing } = state;
+
+  return (
+      <div className="ChatWindow">
+        <Header user={activeUser} />
+        <Chats messages={_.values(messages)} />
+        <MessageInput value={typing} />
+      </div>
     );
   };
 
